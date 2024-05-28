c4ee18ccf5945ec07a49ab233cfdd23f1e528ca5
TimourKhan <timour789@gmail.com>
2024-05-28 16:53:38 -0400

    update

--- index.js
+++ index.js
@@ -11,6 +11,9 @@ const LAD = document.querySelector("#lad-box");
 const LADInfo = document.querySelector(".lad");
 const closeBtn = document.querySelectorAll(".x-button");
 const ourGames = document.querySelector(".our-games");
+const vid = document.querySelector("iframe");
+const content = document.querySelectorAll('.hero, .about-us, .our-games-top');
+
 let visible = false;
 let media = window.matchMedia("(min-width: 900px)").matches;
 
@@ -39,57 +42,85 @@ hamburger.addEventListener("click", () =>{
 })
 
 CK.addEventListener("click", () =>{
-    ourGames.scrollIntoView();
+    CKInfo.style.pointerEvents = "auto";
     CKInfo.style.display = "flex";
+    CKInfo.scrollIntoView();
     setTimeout(function() {
         CKInfo.style.opacity = "1";
         CKInfo.style.transform = "translateY(0%)";
   }, 1);
     fullNav.style.opacity = "0";
-    fullNav.style.pointerEvents = "none";    
+    fullNav.style.pointerEvents = "none";
 })
 RPRT.addEventListener("click", () =>{
-    ourGames.scrollIntoView();
+    RPRTInfo.style.pointerEvents = "auto";
     RPRTInfo.style.display = "flex";
+    RPRTInfo.scrollIntoView();
     setTimeout(function() {
         RPRTInfo.style.opacity = "1";
         RPRTInfo.style.transform = "translateY(0%)";
   }, 1);
     fullNav.style.opacity = "0";
-    fullNav.style.pointerEvents = "none";    
+    fullNav.style.pointerEvents = "none";
 })
 LAD.addEventListener("click", () =>{
-    ourGames.scrollIntoView();
+    LADInfo.style.pointerEvents = "auto";
     LADInfo.style.display = "flex";
+    LADInfo.scrollIntoView();
     setTimeout(function() {
         LADInfo.style.opacity = "1";
         LADInfo.style.transform = "translateY(0%)";
   }, 1);
     fullNav.style.opacity = "0";
-    fullNav.style.pointerEvents = "none";    
+    fullNav.style.pointerEvents = "none";
 });
 
 [...closeBtn].forEach((element) => {
     element.addEventListener('click', (event) => {
+    LADVid.stopVideo();
+    RPRTVid.stopVideo();
+    CKVid.stopVideo();
     CKInfo.style.transform = "translateY(10%)";
     CKInfo.style.opacity = "0";
+    CKInfo.style.pointerEvents = "none";
     setTimeout(function() {
         CKInfo.style.display = "none";
-  }, 300);
+  }, 500);
     RPRTInfo.style.transform = "translateY(10%)";
     RPRTInfo.style.opacity = "0";
+    RPRTInfo.style.pointerEvents = "none";
     setTimeout(function() {
         RPRTInfo.style.display = "none";
-  }, 300);
-      LADInfo.style.transform = "translateY(10%)";
-      LADInfo.style.opacity = "0";
+  }, 500);
+    LADInfo.style.transform = "translateY(10%)";
+    LADInfo.style.opacity = "0";
+    LADInfo.style.pointerEvents = "none";
     setTimeout(function() {
         LADInfo.style.display = "none";
-  }, 300);
+  }, 500);
       fullNav.style.opacity = "1";
       fullNav.style.pointerEvents = "auto";
     });
 });
+
+
+
+
+// this function gets called when API is ready to use
+function onYouTubePlayerAPIReady() {
+    // create the global player from the specific iframe (#video)
+    LADVid = new YT.Player('lad-vid');
+    RPRTVid = new YT.Player('rprt-vid');
+    CKVid = new YT.Player('ck-vid');
+}
+function onPlayerReady() {
+}
+// Inject YouTube API script
+var tag = document.createElement('script');
+tag.src = "https://www.youtube.com/player_api";
+var firstScriptTag = document.getElementsByTagName('script')[0];
+firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
+
 // if (navigator.userAgent.toLowerCase().indexOf('firefox') > -1) {
 //     style.appendChild(document.createTextNode(cssRule));
 //     document.head.appendChild(style);
